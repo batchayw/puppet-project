c4accfc3e96ba534b70a6a4e1a31f3f647d62df1
william <batchayonwilliam@gmail.com>
2025-03-14 02:09:44 +0100

    Unit test for the 'base' profile

---
+++ modules/role/profile/spec/classes/base_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'profile::base' do
+  let(:facts) do
+    {
+      osfamily: 'Debian',
+      operatingsystem: 'Ubuntu',
+      operatingsystemrelease: '20.04',
+    }
+  end
+
+  it { is_expected.to compile.with_all_deps }  # Vérifie que le manifeste compile
+  it { is_expected.to contain_user('deploy') } # Vérifie que l'utilisateur 'deploy' est créé
+end
